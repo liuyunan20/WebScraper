77bba37388fd045b356b35e7b7c65cb145989a44
liuyunan <liuyunan.20@gmail.com>
2022-01-20 00:27:39 -0800

    find the first paragraph with input word

---
+++ Topics/BeautifulSoup  working with HTML/Get the paragraph/main.py
@@ -0,0 +1,13 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+word = input()
+url = input()
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "html.parser")
+paragraphs = soup.find_all("p")
+for p in paragraphs:
+    if word in p.text:
+        print(p.text)
+        break
--- Topics/BeautifulSoup  working with HTML/Health issues/main.py
+++ Topics/BeautifulSoup  working with HTML/Health issues/main.py
@@ -9,6 +9,7 @@ soup = BeautifulSoup(r.content, "html.parser")
 a = soup.find_all("a")
 topic_titles = []
 for i in a:
-    if ("entity" in i.get("href", "no") or "topics" in i.get("href", "no")) and len(i.text) > 1 and i.text.startswith(letter):
-        topic_titles.append(i.text)
+    if ("entity" in i.get("href", "no") or "topics" in i.get("href", "no")):
+        if len(i.text) > 1 and i.text.startswith(letter):
+            topic_titles.append(i.text)
 print(topic_titles)
