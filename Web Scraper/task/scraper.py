2bc2b3908a7aeecbf0b807046cf311c860aebb37
liuyunan <liuyunan.20@gmail.com>
2022-01-20 13:59:58 -0800

    find title and description on webpage

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,9 +1,16 @@
 import requests
+
+from bs4 import BeautifulSoup
+
 print("Input the URL:")
 url = input()
-r = requests.get(url)
-if r.status_code == 200:
-    print(r.json().get("content", "Invalid quote resource!"))
+if "title" in url:
+    response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    soup = BeautifulSoup(response.content, "html.parser")
+    title = soup.find("h1")
+    description = soup.find('span', {'data-testid': 'plot-xl'})
+    t_d = {"title": title.text, "description": description.text}
+    print(t_d)
 else:
-    print("Invalid quote resource!")
-# ["content"]
+    print("Invalid movie page!")
+
