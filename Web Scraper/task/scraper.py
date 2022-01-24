83c2ee70d8acb7a0f2072a3cf2c541f46a1903ae
liuyunan <liuyunan.20@gmail.com>
2022-01-24 11:36:24 -0800

    save webpage content in binary mode

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup
 
 print("Input the URL:")
 url = input()
-if "title" in url:
-    response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-    soup = BeautifulSoup(response.content, "html.parser")
-    title = soup.find("h1")
-    description = soup.find('span', {'data-testid': 'plot-xl'})
-    t_d = {"title": title.text, "description": description.text}
-    print(t_d)
+response = requests.get(url)
+if response.status_code == 200:
+    file = open('source.html', 'wb')
+
+    file.write(response.content)
+    file.close()
+    print("Content saved.")
 else:
-    print("Invalid movie page!")
+    print(f"The URL returned {response.status_code}!")
 
